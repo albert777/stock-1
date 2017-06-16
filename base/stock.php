f80bb9413f29d0608e93703ca4475ccba969bb16
Debondt Didier <p@henix.be>
2017-06-16 10:10:30 +0200

    Commentaire

--- base/stock.php
+++ base/stock.php
@@ -7,6 +7,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 function stock_declarer_tables_objets_sql($tables) {
 
+	// Un champ pour stocker le stock :)
 	$tables['spip_produits']['field']['stock'] = 'bigint(21) NOT NULL DEFAULT 0';
 	$tables['spip_produits']['champs_editables'][] = 'stock';
 	$tables['spip_produits']['champs_versionnes'][] = 'stock';
